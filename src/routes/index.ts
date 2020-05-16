fdacd4cc7bd730463140f1b06497a4b95c37bcbc
lukassxp <lpalucas.10@gmail.com>
2020-05-16 15:10:36 -0300

    setting up typeorm

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
+import usersRouter from './users.routes';
+
 const routes = Router();
 
 routes.get('/', (request, response) =>
   response.json({ message: 'Hello World' }),
 );
+
+routes.use('/users', usersRouter);
 
 export default routes;
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,14 @@
+import 'reflect-metadata';
+
 import express from 'express';
+
+import './database';
+
 import routes from './routes';
 
 const app = express();
 
+app.use(express.json());
 app.use(routes);
 
 app.listen(3333, () => {
